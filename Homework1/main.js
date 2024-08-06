164c9a35722b4e7862dc527fb89b44b289963f47
Misha00512 <misha.kordonetz123@gmail.com>
2024-08-06 08:51:41 +0200

    homework1

---
+++ Homework1/main.js
@@ -0,0 +1,51 @@
+let word1 = 'hello';
+console.log(word1);
+let word2 = 'owu';
+console.log(word2);
+let word3 = 'com';
+console.log(word3);
+let word4 = 'ua';
+console.log(word4);
+
+let num1 = 1;
+console.log(num1);
+let num2 = 10;
+console.log(num2);
+let num3 = -999;
+console.log(num3);
+let num4 = 123;
+console.log(num4);
+let num5 = 3.14;
+console.log(num5);
+let num6 = 2.7;
+console.log(num6);
+let num7 = 16;
+console.log(num7);
+
+let boll1 = true;
+console.log(boll1);
+let boll2 = false;
+console.log(boll2);
+
+
+let firstName = 'Kordonets';
+let middleName = 'Mykhailo';
+let lastName = 'Ryslanovych';
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+
+let Name = prompt();
+console.log(Name);
+let nameFather = prompt();
+console.log(nameFather);
+let Age = prompt();
+console.log(+Age);
